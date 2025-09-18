d54b99f8f507b561fdf65b8b72441c1eeae95b1b
Michael Bestas <mkbestas@lineageos.org>
2025-09-18 22:09:34 +0300

    devon: Switch to AIDL ST NFC HAL
    Change-Id: Ie92ad50f2bb7e98301e1e272dd1c03f93412d9a0

--- device.mk
+++ device.mk
@@ -27,8 +27,9 @@ TARGET_SCREEN_WIDTH := 1080
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.mmi.overlay.rc \
     init.oem.fingerprint2.sh \
-    init.mmi.overlay.rc
+    init.vendor.st21nfc.rc
 
 # LiveDisplay
 $(call soong_config_set,livedisplay_sysfs,enable_ab,true)
@@ -41,7 +42,7 @@ $(call inherit-product, vendor/motorola/MotCamera4-bengal/motcamera4.mk)
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service.st
+    android.hardware.nfc-service.st
 
 # Overlay
 PRODUCT_PACKAGES += \
